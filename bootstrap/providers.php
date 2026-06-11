5bf8cb703c1a05817ec2b271f1774154e1922f7e
Notperss <Adhityanugroho70@gmail.com>
2026-06-11 11:54:54 +0700

    install yajra datatables

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,4 +5,5 @@ return [
     App\Providers\FortifyServiceProvider::class,
     App\Providers\AuthActivityProvider::class,
     App\Providers\HandleMultiLoginProvider::class,
+    Yajra\DataTables\DataTablesServiceProvider::class,
 ];
